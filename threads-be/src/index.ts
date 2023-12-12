06d3f3139e611a4ac1d3f3eb8a3f4b5ffb4c0be5
Muhamad Yoga Atmajaya <myogaatmajaya@gmail.com>
2023-12-12 09:49:26 +0700

    Be

--- threads-be/src/index.ts
+++ threads-be/src/index.ts
@@ -1,5 +1,5 @@
-import * as express from 'express'
-import * as cors from 'cors'
+import express from 'express'
+import cors from 'cors'
 import { AppDataSource } from "./data-source"
 import router from './route'
 
@@ -8,13 +8,13 @@ AppDataSource.initialize()
         const app = express()
         const port = 5002
         const body = require('body-parser')
-        app.use(body.urlencoded({ extended: true })) 
+        app.use(body.urlencoded({ extended: true }))
         app.use(cors())
         app.use(express.json())
-        app.use("/api/v1", router) 
+        app.use("/api/v1", router)
         app.use(express.static('public'))
         app.listen(port, () => {
             console.log("Server running on port: " + port)
         })
     })
-.catch(error => console.log(error))
+    .catch(error => console.log(error))
