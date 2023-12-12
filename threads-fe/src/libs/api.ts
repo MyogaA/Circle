99a616226f7d7b9291ebf47a76dd96f41a4683cb
Muhamad Yoga Atmajaya <myogaatmajaya@gmail.com>
2023-12-12 10:26:59 +0700

    vercel

--- threads-fe/src/libs/api.ts
+++ threads-fe/src/libs/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const API = axios.create({
-  baseURL: "http://localhost:5002/api/v1"
+  baseURL: "https://circle-myogaa.vercel.app/api/v1"
 })
 
 export function setAuthToken(token: string  | any) {
